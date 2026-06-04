1dbb39e396cad47e5aa966ba786acd52ffa455dc
vanshshar <vanshsh2327@gmail.com>
2026-06-04 16:08:25 +0530

    day 2 done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,44 @@
+// import { response } from "express";
+import React from "react"
+import { useState } from "react";
+
 function App() {
+
+  const [student , setStudent] = useState(null);
+
+  const getStudent = async () => {
+    console.log("button clicked");
+    const response = await fetch("http://localhost:8081/student");
+    console.log(response);
+
+    const data = await response.json();
+    console.log(data);
+    setStudent(data);
+  }
+
   return(
-    <div>
-      <h1>hello</h1>
+     <div style={{ padding: "40px" }}>
+
+      <button onClick={getStudent}>
+        Get Student
+      </button> 
+
+      {
+        student && (
+          <div>
+
+            <h2>ID : {student.id}</h2>
+
+            <h2>Name : {student.name}</h2>
+
+            <h2>Course : {student.email}</h2>
+
+          </div>
+        )
+      }
+
     </div>
   )
 }
+
+export default App;
